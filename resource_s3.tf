da6307ca09372c0008d7c406b9c02a35fb87af12
Jay S <49022635+sjraju@users.noreply.github.com>
2022-05-31 15:12:26 +0530

    Update resource_s3.tf

--- resource_s3.tf
+++ resource_s3.tf
@@ -1,6 +1,6 @@
 ############### aws s3 resource ##############
 resource "aws_s3_bucket" "mybucket" {
-bucket = var.s3_bucket_name
+bucket = jay.s3_bucket_name
 tags = {
 env = lookup(var.s3_tags, "environment")
 }
